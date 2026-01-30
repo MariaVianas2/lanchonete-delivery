7a024b0622655b780a1fc329bb2000d3681e5efd
Eduarda <eduarda321viana@gmail.com>
2026-01-30 19:52:02 -0300

    Criação da classe + criação da tabela

--- backend/src/main/java/com/eduarda/lanchonete/model/Produto.java
+++ backend/src/main/java/com/eduarda/lanchonete/model/Produto.java
@@ -1,14 +1,23 @@
 package com.eduarda.lanchonete.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="produtos")
 public class Produto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String descricao;
     private double preco;
 
     public Produto(){
-
     }
 
     public Produto(Long id, String nome, String descricao, double preco){
